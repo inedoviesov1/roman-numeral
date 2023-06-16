86b66c2366c53c02ec446421edb3e69a44b5d0d1
Ievgen Nedoviesov <nedovesov.ea@gmail.com>
2023-06-16 10:30:41 -0700

    Add a sanity check for the whole Spring Boot App

--- src/test/java/com/inedoviesov1/romannumeral/RomanNumeralApplicationTests.java
+++ src/test/java/com/inedoviesov1/romannumeral/RomanNumeralApplicationTests.java
@@ -1,13 +1,23 @@
 package com.inedoviesov1.romannumeral;
 
+import com.inedoviesov1.romannumeral.controller.RomanNumeralController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class RomanNumeralApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private RomanNumeralController romanNumeralController;
+
+    // Sanity check to ensure that the application context can start and required controllers are registered.
+    @Test
+    void contextLoads() {
+        assertThat(romanNumeralController).isNotNull();
+    }
 
 }
